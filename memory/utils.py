b00545c92aade131895d95095894f60261d8eeca
Haim Aflalo <kodkodhaim@gmail.com>
2025-10-28 10:55:37 +0200

    function create_game

--- memory/utils.py
+++ memory/utils.py
@@ -1,10 +1,11 @@
+import random
 def create_matrix(size,fill = 0):
     mat = []
     for i in range(size):
-        lst = []
+        col = []
         for j in range(size):
-            lst.append(fill)
-        mat.append(lst)
+            col.append(fill)
+        mat.append(col)
     return mat
 
 def create_cards(size):
@@ -13,3 +14,12 @@ def create_cards(size):
         cards.append(i)
         cards.append(i)
     return cards
+
+def create_game(mat,card_lst):
+    print(card_lst)
+    for i in range(len(mat)):
+        for j in range(len(mat)):
+            card = random.choice(card_lst)
+            mat[i][j] = card
+            card_lst.remove(card)
+    return mat
